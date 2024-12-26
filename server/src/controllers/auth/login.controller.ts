8186ec4546a5602bba8f1a34baae92b5b35ffcd9
spalnique <lex.sydoruk@gmail.com>
2024-12-26 13:47:01 +0100

    minor fix

--- server/src/controllers/auth/login.controller.ts
+++ server/src/controllers/auth/login.controller.ts
@@ -3,34 +3,31 @@ import { RequestHandler } from 'express';
 import findUser from '../../services/user/find.service';
 import createToken from '../../utils/createToken';
 
-const loginController: RequestHandler = async (req, res, next) => {
-  if (
-    req.session.email === req.body.email &&
-    req.get('Authorization') === `Bearer ${req.session.accessToken}`
-  ) {
-    req.session.touch();
-    req.session.save();
+const loginController: RequestHandler = async (req, res, _next) => {
+  // if (
+  //   req.session.email === req.body.email &&
+  //   req.get('Authorization') === `Bearer ${req.session.accessToken}`
+  // ) {
+  //   req.session.touch();
+  //   req.session.save();
 
-    res.status(200).json({ status: 200, message: 'Already logged in' });
-    return;
-  }
+  //   res.status(200).json({ status: 200, message: 'Already logged in' });
+  //   return;
+  // }
 
   const user = await findUser(req.body);
 
-  const accessToken = createToken(user.id);
+  const token = createToken(user.id);
 
   Object.assign(req.session, {
     email: user.email,
-    accessToken,
+    token,
   });
 
   res.status(200).json({
     status: 200,
     message: 'Successfully logged in',
-    data: {
-      user,
-      accessToken,
-    },
+    data: { user, token },
   });
 };
 
