eff6ceff19f035d2b993a5d04b64791a7cdef730
spalnique <lex.sydoruk@gmail.com>
2025-01-06 16:46:16 +0100

    samesite: "none"

--- server/src/middleware/session.ts
+++ server/src/middleware/session.ts
@@ -14,6 +14,7 @@ const session = expressSession({
   name: SessionCookie.name,
   cookie: {
     httpOnly: true,
+    sameSite: 'none',
     maxAge: WEEK,
     secure: env(ENV.PROD_ENV) === 'true' ? true : false,
   },
