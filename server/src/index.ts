8a8a7b89489fc1ec20139edbf1e6343f7688aee5
spalnique <lex.sydoruk@gmail.com>
2025-01-11 17:11:03 +0100

    refactor for single port usage

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,18 +4,18 @@ import { ENV_VARS } from '@constants';
 import { connectToDB } from '@db';
 import { env } from '@helpers';
 
-import app from './app';
+// import app from './app';
 import httpServer from './socket';
 
 (async () => {
   const PORT = env(ENV_VARS.PORT, 3000);
-  const SOCKET_PORT = env(ENV_VARS.SOCKET_PORT, 4004);
+  // const SOCKET_PORT = env(ENV_VARS.SOCKET_PORT, 4004);
 
   await connectToDB();
 
-  app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+  // app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
 
-  httpServer.listen(SOCKET_PORT, () => {
-    console.log(`HTTP server running on http://localhost:${SOCKET_PORT}`);
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
   });
 })();
